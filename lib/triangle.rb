0e0acf2853739c5539b085f804d9940786873e75
Devon Wehrlin <dkwehrlin.99@gmail.com>
2020-07-01 21:19:13 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,19 @@
 class Triangle
-  # write code here
+
+  def initialize(side_1, side_2, side_3)
+    @side_1 = side_1
+    @side_2 = side_2 
+    @side_3 = side_3 
+  end 
+
+  def kind 
+    if side_1 == side_2 && side_1 == side_3 
+      return :equilateral
+    elsif @side_1 == @side_2 || @side_2 == @side_3 || @side_3 == @side_1 
+      return :isosceles
+    else 
+      reuturn :scalene
+    end 
+  end 
+    
 end
