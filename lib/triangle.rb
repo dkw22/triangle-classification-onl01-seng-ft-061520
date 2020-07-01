833c69b0738663a5a0c09c33884b5ac003e42d5a
Devon Wehrlin <dkwehrlin.99@gmail.com>
2020-07-01 21:19:45 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,7 +12,7 @@ class Triangle
     elsif @side_1 == @side_2 || @side_2 == @side_3 || @side_3 == @side_1
       return :isosceles
     else
-      reuturn :scalene
+      return :scalene
     end
   end
 
