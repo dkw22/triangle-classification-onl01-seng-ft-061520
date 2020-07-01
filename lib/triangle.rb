0aa9f31dab2b280e3864042cc5df83335ae561bd
Devon Wehrlin <dkwehrlin.99@gmail.com>
2020-07-01 21:19:32 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,13 +7,13 @@ class Triangle
   end
 
   def kind
-    if side_1 == side_2 && side_1 == side_3
+    if @side_1 == @side_2 && @side_1 == @side_3
       return :equilateral
     elsif @side_1 == @side_2 || @side_2 == @side_3 || @side_3 == @side_1
       return :isosceles
     else
       reuturn :scalene
     end
-  end 
+  end
 
 end
